25835ed14928df89f537e17182024bb0fea6e682
Jon's <jon.rod2405@gmail.com>
2026-09-20 18:13:30 +0000

    feat (hero): adiciona efeito parallax
    feat (nav): adiciona alteração de cor com base no hero

--- src/js/useracess.js
+++ src/js/useracess.js
@@ -5,3 +5,36 @@ function quero_doar() { // Função para redirecionar o usuário para a página 
     window.location.href = "login_regis.html";
 }
 
+
+// Efeito parallax na seção hero
+const parallax = document.querySelectorAll('.parallax');
+
+document.addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX / window.innerWidth - 0.5;
+
+    parallax.forEach((item) => {
+        const velocidade = Number(item.dataset.speed);
+
+        const x = mouseX * velocidade;
+
+        item.style.transform = `translate(${x}px, 0px)`;
+    });
+});
+
+
+
+// Muda cor da barra de navegação quando entra ou sai na seção hero
+const heroSection = document.querySelector('.hero-section');
+const nav = document.querySelector('nav');
+
+const observer = new IntersectionObserver(
+    ([entry]) => {
+        nav.classList.toggle('nav-scrolled', !entry.isIntersecting);
+    },
+    {
+        threshold: 0,
+        rootMargin: `-${nav.offsetHeight}px 0px 0px 0px`
+    }
+);
+
+observer.observe(heroSection);
